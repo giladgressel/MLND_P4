9a0b749d731fa7928da1d21369ecd8c8a0817508
GiladGressel <giladgressel@gmail.com>
2016-04-17 13:32:01 -0700

    updated cab to have pygame as optional
    display is now optional.  copy pasted from UDacitys machine learning github where the changes already existed
    changes made in environment, agent and simulator.

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -29,8 +29,11 @@ class LearningAgent(Agent):
 
     #for the current state, iterates over all possible actions and returns the action which maximizes the Q value
     def ArgMAX_Q (self, state):
-        return max(self.Q_table[state].iteritems(), key= lambda x : x[1])[0]
-
+        #if the sum of the values is greater than 0, then there is an entry, so choose the max
+        if sum(self.Q_table[state].values()) != 0:
+               return max(self.Q_table[state].iteritems(), key= lambda x : x[1])[0]
+        else: #otherwise, just pick a random action.  We have to force this because max doesn't randomly break ties, it will choose the same option again and again for different unseen states.
+            return random.choice(self.valid_actions)
 
     #for the current state, iterates over all possible actions and returns the largest Q value
     def MAX_Q (self, state):
@@ -47,7 +50,7 @@ class LearningAgent(Agent):
         # TODO: Update state
 
         #this state will act as a key in the Q-table dictionary
-        self.state = (('time_left', deadline), ('light', inputs['light'] ), ('next_waypoint', self.next_waypoint))
+        self.state = (('light', inputs['light'] ), ('next_waypoint', self.next_waypoint))
 
         #initialize the state into the Q_table will do nothing if it's already there.
         if self.state not in self.Q_table:
@@ -67,9 +70,9 @@ class LearningAgent(Agent):
 
         #sense environment again,  in order to update Q_table.
         inputs_2 = self.env.sense(self)
-        deadline -= 1  #we have to force this because the deadline only updates with env.step(), it won't update from env.act().
+        #deadline -= 1  #we have to force this because the deadline only updates with env.step(), it won't update from env.act().
         self.next_waypoint = self.planner.next_waypoint()
-        state_2 = (('time_left', deadline), ('light', inputs_2['light'] ), ('next_waypoint', self.next_waypoint))
+        state_2 = (('light', inputs_2['light'] ), ('next_waypoint', self.next_waypoint))
 
         #initialize the new state into the Q_table will do nothing if it's already there.
         if state_2 not in self.Q_table:
@@ -84,6 +87,7 @@ class LearningAgent(Agent):
 
 
 
+
         #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
 
@@ -93,11 +97,15 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    # NOTE: You can set enforce_deadline=False while debugging to allow longer trials+    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
 
     # Now simulate it
-    sim = Simulator(e, update_delay=1.0)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=100)  # press Esc or close pygame window to quit
+    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    # NOTE: To speed up simulation, reduce update_delay and/or set display=False
+
+    sim.run(n_trials=100)  # run for a specified number of trials
+    # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
 
 if __name__ == '__main__':
