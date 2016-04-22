4a6836f6f1c2ea8fb999bd3b67035d855a3db3ff
GiladGressel <giladgressel@gmail.com>
2016-04-22 10:01:08 -0700

    edited plotting, added pdf report

--- smartcab/smartcab/plotting.py
+++ smartcab/smartcab/plotting.py
@@ -4,16 +4,17 @@ import matplotlib.pyplot as plt
 
 
 grid_values = np.arange(0,1.1,0.2)
-f, axarr = plt.subplots(6,6, figsize = (60,30),sharex='col', sharey= 'row')
+f, axarr = plt.subplots(6,6, figsize = (60,60),sharex='col', sharey= 'row')
 
 for x,alpha in enumerate(grid_values):
     for y,gamma in enumerate(grid_values):
         data = pd.read_csv("Alpha={}_Gamma={}.csv".format(alpha,gamma))
-
-        #plt.figure(figsize = (28,28))
         axarr[x,y].bar(np.arange(100),data['Net-Reward'], color = 'b')
         axarr[x,y].bar(np.arange(100),data['Reached'], color = 'r')
-        axarr[x,y].set_title("Alpha={},Gamma={}".format(alpha,gamma))
+        axarr[x,y].text(.5,.9,"Alpha={},Gamma={}".format(alpha,gamma),horizontalalignment='center',
+                        transform=axarr[x,y].transAxes, size = 40)
+        axarr[x,y].tick_params(axis = 'both', which='both', labelsize = 25)
+
 
 f.subplots_adjust(hspace=0.05, wspace = 0.05)
 plt.show()
